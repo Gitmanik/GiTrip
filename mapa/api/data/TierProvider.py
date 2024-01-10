f2d596f3325148868ed16f38bc3f1b5fea779949
Paweł Reich <37516105+Gitmanik@users.noreply.github.com>
2024-01-11 00:01:40 +0100

    wyswietlanie mevo i tier pobieranie

--- mapa/api/data/TierProvider.py
+++ mapa/api/data/TierProvider.py
@@ -11,6 +11,21 @@ class TierProvider:
         api = {'x-api-key': 'bpEUTJEBTf74oGRWxaIcW7aeZMzDDODe1yBoSxi2'}
         r = requests.get(f"https://platform.tier-services.io/v1/vehicle?lat={lat}&lng={lng}&radius={radius}", headers = api)
 
-        return r.json()
+        all_scooters = r.json()
+
+        to_ret = list()
+
+        for scooter in all_scooters:
+            if not scooter['is_rentable']:
+                continue
+            to_ret.append({
+                'id': scooter['id'],
+                'max_speed': scooter['max_speed'],
+                'lat': scooter['lat'],
+                'lng': scooter['lng'],
+                'range': scooter['currentRangeMeters']
+            })
+
+        return to_ret
 
     
