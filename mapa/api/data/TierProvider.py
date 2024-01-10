d7ecd08f25a703dbe5d99084b1288e13bf560488
Paweł Reich <37516105+Gitmanik@users.noreply.github.com>
2024-01-10 17:13:51 +0100

    basic tier provider

---
+++ mapa/api/data/TierProvider.py
@@ -0,0 +1,14 @@
+import requests
+
+
+class TierProvider:
+
+    def __init__(self):
+        pass
+
+    # radius w metrach?
+    def get_scooters_data(self, lat, lng, radius):
+        api = {'x-api-key': 'bpEUTJEBTf74oGRWxaIcW7aeZMzDDODe1yBoSxi2'}
+        r = requests.get(f"https://platform.tier-services.io/v1/vehicle?lat={lat}&lng={lng}&radius={radius}", headers = api)
+
+        return r.json()
--- mapa/main.py
+++ mapa/main.py
@@ -1,10 +1,17 @@
 from flask import Flask, render_template
 import api.maps.GoogleMapsProvider
+import api.data.TierProvider
 
 gmaps = api.maps.GoogleMapsProvider.GoogleMapsProvider('AIzaSyDsP9RqZORUaJlsX3f1zGJqDJccBNXez4o')
+
+tier = api.data.TierProvider.TierProvider()
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
     return render_template('index.html', gmaps=gmaps.get_map())
+
+@app.route("/tiertest")
+def tier_test():
+    return tier.get_scooters_data(54.372158, 18.638306, 1000)
