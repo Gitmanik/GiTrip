3272d835161b3db116d3eb5ed92ba92b829cf5b4
Paweł Reich <37516105+Gitmanik@users.noreply.github.com>
2024-01-10 15:31:02 +0100

    usuneicie folderu idea

--- mapa/main.py
+++ mapa/main.py
@@ -1,8 +1,10 @@
 from flask import Flask, render_template
+import api.maps.GoogleMapsProvider
+
+gmaps = api.maps.GoogleMapsProvider.GoogleMapsProvider('AIzaSyDsP9RqZORUaJlsX3f1zGJqDJccBNXez4o')
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return render_template('index.html')
-    
+    return render_template('index.html', gmaps=gmaps.get_map())
