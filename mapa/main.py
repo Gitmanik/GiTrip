e41c0200868f3b30138a9801c096cad42fdc786c
wujoq <130597953+wujoq@users.noreply.github.com>
2024-01-10 19:19:23 +0100

    comm

--- mapa/main.py
+++ mapa/main.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+
+from flask import Flask, render_template, request, jsonify
 import api.maps.GoogleMapsProvider
 
 gmaps = api.maps.GoogleMapsProvider.GoogleMapsProvider('AIzaSyDsP9RqZORUaJlsX3f1zGJqDJccBNXez4o')
@@ -8,3 +9,15 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return render_template('index.html', gmaps=gmaps.get_map())
+
+@app.route("/nasze-api", methods=["POST"])
+def nasze_api():
+    try:
+        input_value = request.form.get('input')
+        # Do something with the input_value, for example, process it and return a response
+        response_data = {'message': 'Request received successfully', 'input': input_value}
+        return jsonify(response_data), 200
+    except Exception as e:
+        error_message = f'Error processing request: {str(e)}'
+        return jsonify({'error': error_message}), 500
+
