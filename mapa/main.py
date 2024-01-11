14eea60b0125c35c5827efacc765b617dd55a4f8
Pawel Reich <thegitman@outlook.com>
2024-01-11 14:58:26 +0100

    apparently it does not work like that

--- mapa/main.py
+++ mapa/main.py
@@ -14,14 +14,17 @@ mevo = api.data.MevoProvider.MevoProvider()
 tier = api.data.TierProvider.TierProvider()
 
 app = Flask(__name__)
-if os.getenv('app_root'):
-    app.config["APPLICATION_ROOT"] = os.getenv('app_root')
 
-@app.route("/")
+base_url = ''
+
+if os.getenv('app_root'):
+    base_url = os.getenv('app_root')
+
+@app.route(base_url + "/")
 def hello_world():
     return render_template('index.html', gmaps=gmaps.get_map())
 
-@app.route("/api/autocomplete", methods=["POST"])
+@app.route(base_url + "/api/autocomplete", methods=["POST"])
 def api_autocomplete():
     try:
         data = json.loads(request.data)
@@ -32,15 +35,15 @@ def api_autocomplete():
         error_message = f'Error processing request: {str(e)}, {traceback.format_exc()}'
         return jsonify({'error': error_message}), 500
 
-@app.route("/api/allbikes")
+@app.route(base_url + "/api/allbikes")
 def api_allbikes():
     return mevo.get_all_bikes()
 
-@app.route("/api/allbikeparkings")
+@app.route(base_url + "/api/allbikeparkings")
 def api_allbikeparkings():
     return mevo.get_all_bike_parkings()
 
-@app.route("/api/get_path", methods=["POST"])
+@app.route(base_url + "/api/get_path", methods=["POST"])
 def api_getpath():
     try:
         data = json.loads(request.data)
